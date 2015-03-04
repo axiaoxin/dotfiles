f6fbd6066d7f93b18c8d37a692d4cafb1068f9fa
axiaoxin <254606826@qq.com>
2015-03-04 18:16:45 +0800

    minor

--- install.sh
+++ install.sh
@@ -5,12 +5,9 @@ ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/
 
 echo "=> homebrew update"
 brew update &>/dev/null
-echo "=> install bins by homebrew"
-brew install htop unrar tree ctags ssh-copy-id wget coreutils git-flow vim ffmpeg figlet gist &>/dev/null
-
-echo "=> gem update"
 sudo gem update --system &>/dev/null
-echo "=> install lolcat"
+echo "=> install bins"
+brew install htop unrar tree ctags ssh-copy-id wget coreutils git-flow vim ffmpeg figlet gist &>/dev/null
 sudo gem install lolcat &>/dev/null
 
 echo "=> install python modules"
@@ -18,7 +15,7 @@ sudo easy_install pip &>/dev/null
 sudo pip install ipython jedi virtualenv virtualenvwrapper pyflakes pylint autopep8 shadowsocks  gevent pyperclip pbkdf2 Pygments pythonpy unp simpletornadoserver &>/dev/null
 
 echo "=> install omyzsh"
-curl -L http://install.ohmyz.sh | sh &>/dev/null
+curl -L http://install.ohmyz.sh | sh
 
 echo "=> make softlinks"
 
