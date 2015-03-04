1936faec187c82a3f68fb4c7fd53935db54fa0c2
axiaoxin <254606826@qq.com>
2015-03-04 18:13:52 +0800

    silence

--- install.sh
+++ install.sh
@@ -6,24 +6,48 @@ ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/
 echo "=> homebrew update"
 brew update &>/dev/null
 echo "=> install bins by homebrew"
-brew install htop unrar tree ctags ssh-copy-id wget coreutils git-flow vim ffmpeg figlet gist
+brew install htop unrar tree ctags ssh-copy-id wget coreutils git-flow vim ffmpeg figlet gist &>/dev/null
 
 echo "=> gem update"
 sudo gem update --system &>/dev/null
 echo "=> install lolcat"
-sudo gem install lolcat
+sudo gem install lolcat &>/dev/null
 
 echo "=> install python modules"
-easy_install pip
+sudo easy_install pip &>/dev/null
 sudo pip install ipython jedi virtualenv virtualenvwrapper pyflakes pylint autopep8 shadowsocks  gevent pyperclip pbkdf2 Pygments pythonpy unp simpletornadoserver &>/dev/null
 
 echo "=> install omyzsh"
-curl -L http://install.ohmyz.sh | sh
+curl -L http://install.ohmyz.sh | sh &>/dev/null
 
 echo "=> make softlinks"
-ln -s `pwd`/.gitconfig ~/.gitconfig
-ln -s `pwd`/.inputrc ~/.inputrc
-ln -s `pwd`/.vimrc ~/.vimrc
-ln -s `pwd`/.zshrc ~/.zshrc
+
+if [ -d ~/.pip ]; then
+    rm -rf ~/.pip
+fi
 ln -s `pwd`/.pip ~/.pip
+
+if [ -d ~/.vim ]; then
+    rm -rf ~/.vim
+fi
 ln -s `pwd`/.vim ~/.vim
+
+if [ -f ~/.gitconfig ]; then
+    rm -rf ~/.gitconfig
+fi
+ln -s `pwd`/.gitconfig ~/.gitconfig
+
+if [ -f ~/.inputrc ]; then
+    rm -rf ~/.inputrc
+fi
+ln -s `pwd`/.inputrc ~/.inputrc
+
+if [ -f ~/.vimrc ]; then
+    rm -rf ~/.vimrc
+fi
+ln -s `pwd`/.vimrc ~/.vimrc
+
+if [ -f ~/.zshrc ]; then
+    rm -rf ~/.zshrc
+fi
+ln -s `pwd`/.zshrc ~/.zshrc
