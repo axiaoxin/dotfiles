3bf4c9cb8a195701cf286df9f5330945106d00a9
axiaoxin <254606826@qq.com>
2015-03-04 17:14:27 +0800

    >/dev/null

--- install.sh
+++ install.sh
@@ -2,14 +2,21 @@
 
 ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 
-curl -L http://install.ohmyz.sh | sh
-
-brew update
+brew update >/dev/null 2>&1
 brew install htop unrar tree ctags ssh-copy-id wget coreutils git-flow vim ffmpeg
 
-sudo gem update --system
+sudo gem update --system >/dev/null 2>&1
 sudo gem install lolcat
 
 easy_install pip
-curl -L https://raw.githubusercontent.com/yyuu/pyenv-installer/master/bin/pyenv-installer | bash
 sudo pip install ipython jedi virtualenv virtualenvwrapper pyflakes pylint autopep8 shadowsocks  gevent pyperclip pbkdf2 Pygments pythonpy unp
+
+curl -L http://install.ohmyz.sh | sh
+
+ln -s `pwd`/.gitconfig ~/.gitconfig
+ln -s `pwd`/.inputrc ~/.inputrc
+ln -s `pwd`/.vimrc ~/.vimrc
+ln -s `pwd`/.zshrc ~/.zshrc
+ln -s `pwd`/.pip ~/.pip
+ln -s `pwd`/.vim ~/.vim
+sudo ln -s `pwd`/wgetpaste /bin/wgetpaste
