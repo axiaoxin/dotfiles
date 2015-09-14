de1b46be7c108c54991c20166f0a05afbe9c5c62
axiaoxin <254606826@qq.com>
2015-09-14 18:27:00 +0800

    install ack

--- install.sh
+++ install.sh
@@ -14,7 +14,7 @@ sudo gem update --system &>/dev/null
 
 echo "=> install bins"
 brew install vim --with-lua
-brew install git htop npm unrar tree ssh-copy-id wget coreutils ffmpeg figlet gist ctags cheat &>/dev/null
+brew install git htop npm unrar tree ssh-copy-id wget coreutils ffmpeg figlet gist ctags cheat ack &>/dev/null
 sudo gem install lolcat &>/dev/null
 sudo npm install -g bower &>/dev/null
 sudo npm install -g ttyrec ttycast &>/dev/null
