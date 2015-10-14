ab5d86723a51acd79e75612f65d80c9fc0b1c5dc
axiaoxin <254606826@qq.com>
2015-10-14 14:08:55 +0800

    mojibar brew cask

--- install.sh
+++ install.sh
@@ -13,8 +13,10 @@ brew update &>/dev/null
 sudo gem update --system &>/dev/null
 
 echo "=> install bins"
+brew install caskroom/cask/brew-cask
 brew install vim --with-lua
 brew install git htop npm unrar tree ssh-copy-id wget coreutils ffmpeg figlet gist ctags cheat the_silver_searcher &>/dev/null
+brew cask install mojibar &>/dev/null
 sudo gem install lolcat &>/dev/null
 sudo npm install -g bower &>/dev/null
 sudo npm install -g ttyrec ttycast &>/dev/null
