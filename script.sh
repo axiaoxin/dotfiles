098d65e7fe52a24a9fb3a4f459ea9dba3bd7e21b
axiaoxin <254606826@qq.com>
2019-10-08 18:46:45 +0800

    Update script.sh

--- script.sh
+++ script.sh
@@ -1,2 +1,21 @@
 /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 brew update
+
+curl -L http://install.ohmyz.sh | sh
+
+defaults write NSGlobalDomain AppleShowAllExtensions -bool true
+defaults write com.apple.finder AppleShowAllFiles true
+defaults write com.apple.finder _FXShowPosixPathInTitle -bool true
+defaults write com.apple.Safari IncludeInternalDebugMenu -bool true && \
+defaults write com.apple.Safari IncludeDevelopMenu -bool true && \
+defaults write com.apple.Safari WebKitDeveloperExtrasEnabledPreferenceKey -bool true && \
+defaults write com.apple.Safari com.apple.Safari.ContentPageGroupIdentifier.WebKit2DeveloperExtrasEnabled -bool true && \
+defaults write NSGlobalDomain WebKitDeveloperExtras -bool true
+defaults write com.apple.TextEdit RichText -int 0
+defaults write com.apple.TimeMachine DoNotOfferNewDisksForBackup -bool true
+chflags nohidden ~/Library
+defaults write com.apple.finder ShowPathbar -bool true
+defaults write com.apple.finder ShowStatusBar -bool true
+
+# .zhsrc
+# download app
